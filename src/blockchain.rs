62dbede8230677432adef5316cd1160f22048fe7
Jeremie Bonsant <jeremie@bonsant.ca>
2022-09-22 06:52:01 -0400

    link CLI to the node

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,19 +1,65 @@
-use spin_sleep::LoopHelper;
 
-const INTERVAL_IN_SECONDS: 	f64 = 2.0;
-const RATE_PER_SECOND: 		f64 = 1.0 / INTERVAL_IN_SECONDS;
-
-pub fn run() {
-
-	let mut loop_helper = LoopHelper::builder().build_with_target_rate(RATE_PER_SECOND);
+const GENESIS_ACCOUNT_ID: u32 = u32::MAX;
 
 
-	println!("Blockchain node started");
 
-	loop {
-		loop_helper.loop_start(); // or .loop_start_s() for f64 seconds
+/// A single transaction between a sender and recipient
+pub struct Transaction {
+    pub sender: u32,
+    pub recipient: u32,
+    pub amount: u32
+}
 
-    	loop_helper.loop_sleep(); 
-		eprint!(".");
+/// A block on the chain, storing a vector of transactions and proof of work
+pub struct Block {
+    pub index: usize,
+    pub transactions: Vec<Transaction>,
+    pub proof: u64,
+    pub previous_hash: String
+}
+
+pub struct Blockchain {
+    pub chain: Vec<Block>,
+    pub current_transactions: Vec<Transaction>,
+}
+
+impl Transaction {
+    fn new(sender: u32, recipient: u32, amount: u32) -> Transaction {
+        Transaction { sender, recipient, amount }
+    }
+}
+
+impl Block {
+    fn new(previous_hash: String, proof: u64) -> Block {
+        Block { index: 0, transactions: Vec::new(), proof: proof, previous_hash: previous_hash }
+    }
+}
+
+impl Blockchain {
+    /// Create a new chain with two sender accounts credited 5000 of the token
+    pub fn new() -> Blockchain {
+        let first_hash = "".to_string();
+        let first_proof = 100;
+        let mut first_block = Block::new(first_hash, first_proof);
+        let genesis_transaction_1 = Transaction::new(GENESIS_ACCOUNT_ID, 1000, 5000);
+        let genesis_transaction_2 = Transaction::new(GENESIS_ACCOUNT_ID, 1001, 5000);
+        first_block.transactions = vec![genesis_transaction_1, genesis_transaction_2];
+        Blockchain { chain: vec![first_block], current_transactions: Vec::new() }
+	}
+
+	pub fn new_block(&self) {
+		println!("blockchain : new_block");
+	}
+
+	pub fn new_account(&self, account: u32, amount: u32) {
+
+	}
+	
+	pub fn new_transaction(&self, sender: u32, recipient: u32, amount: u32) {
+
+	}
+
+	pub fn get_balance(&self, account: u32) -> u32 {
+		return 77;
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use clap::{Parser, Subcommand};
 mod blockchain;
+use crate::blockchain::Blockchain;
+use tokio::net::UdpSocket;
+use tokio::time::{Duration};
+use std::io;
+use std::error::Error;
+use bytes::{Bytes, BytesMut, BufMut};
+
+const NODE_ADDR: &str = "0.0.0.0:8080";
+const CLIENT_ADDR: &str = "0.0.0.0:8081";
 
 #[derive(Parser)]
 #[clap(name = "b")]
@@ -27,25 +36,115 @@ enum Commands {
 	},
 }
 
-fn main() {
+fn args_to_bytes(args: std::env::Args) -> Vec<u8> {
+
+	let mut bytes = BytesMut::new();
+	
+	for arg in args {
+		bytes.put(format!("{}{}", arg, '\0').as_bytes());
+	}
+
+	bytes.to_vec()
+}
+
+fn bytes_to_args(bytes: Bytes) -> Vec<String> {
+
+	let mut args: Vec<String> = Vec::new();
+
+	for item in bytes.split(|c| c == &(0)) {
+		if item.len() > 0 {
+			args.push(String::from_utf8_lossy(item).to_string());
+		}
+	}
+
+	args
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+	let mut rcv_data = [0; 1024];
 	let cli = Cli::parse();
 
-	match &cli.command {
+	match cli.command {
 
-		Commands::StartNode { } => {
+		Commands::StartNode {} => {
+
 			println!("Starting Node ...");
-			blockchain::run();
-		},
-		Commands::CreateAccount { id_of_account, starting_balance } => {
-			println!("Creating Account\nid: {:?}\nbalance: {:?}\n...", id_of_account, starting_balance);
-		},
-		Commands::Transfert { from_account , to_account, ammount } => {
-			println!("Transferring funds \nfrom: {:?}\nto: {:?}\nammount: {:?}\n...", from_account, to_account, ammount);
-		},
-		Commands::Balance { account } => {
-			let ammount: u32 = 3;
-			println!("Balance of account '{:?}' is {:?}", account, ammount);
-		},
 
+			let chain = Blockchain::new();
+			match UdpSocket::bind(NODE_ADDR).await {
+				Err(e) => {
+					if e.kind() == io::ErrorKind::AddrInUse {
+						println!("Node already started !")
+					}
+					return Err(e.into());
+				}
+				Ok(socket) => {
+					loop {
+						tokio::select! {
+							_ = tokio::time::sleep(Duration::from_secs(2)) => {
+								chain.new_block();
+							}
+							Result::Ok((len, addr)) = socket.recv_from(&mut rcv_data) => {
+							
+								let bytes = Bytes::copy_from_slice(&rcv_data[..len]);
+								let args = bytes_to_args(bytes);
+
+								let cli = Cli::parse_from(args.into_iter());
+
+								match cli.command {
+									Commands::CreateAccount { id_of_account, starting_balance } => {
+
+										chain.new_account(id_of_account, starting_balance);
+
+										let msg = format!("Account created");
+										socket.send_to(msg.as_bytes(), addr).await?;
+									},
+									Commands::Transfert { from_account , to_account, ammount } => {
+
+										chain.new_transaction(from_account, to_account, ammount);
+
+										let msg = format!("Transaction done");
+										socket.send_to(msg.as_bytes(), addr).await?;
+									},
+									Commands::Balance { account } => {
+
+										let ammount: u32 = chain.get_balance(account);
+										
+										let msg = format!("Balance of account '{:?}' is {:?}", account, ammount);
+										socket.send_to(msg.as_bytes(), addr).await?;
+									}
+									_ => {}
+								}	
+							}
+						}
+					}
+				}
+			}
+		},
+		_ => {
+
+			let socket = UdpSocket::bind(CLIENT_ADDR).await?;
+			socket.connect(NODE_ADDR).await?;
+
+			match cli.command {
+				Commands::CreateAccount { id_of_account, starting_balance } => {
+					println!("Creating Account : id = {:?}, balance = {:?}\n...", id_of_account, starting_balance);
+				},
+				Commands::Transfert { from_account , to_account, ammount } => {
+					println!("Transferring funds : from = {:?}, to = {:?}, ammount = {:?}\n...", from_account, to_account, ammount);
+				},
+				_ => {}
+			}
+
+			let args_data = args_to_bytes(std::env::args());
+			socket.send(&args_data).await?;
+
+			let len = socket.recv(&mut rcv_data).await?;
+			println!("{}", String::from_utf8_lossy(&rcv_data[..len]));
+		}
 	}
+
+	Ok(())
 }
